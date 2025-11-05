7b86facdabf665ff5bce9767adbd43beef560771
Tejas300102 <tejasjadhao121@gmail.com>
2025-11-05 17:51:42 +0530

    home page with getPolicy apis is done

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,12 @@ import { registerUser } from "./src/controller/UserController.js";
 import { login } from "./src/controller/LoginController.js";
 import { getAllUsers, getUserPolicies } from "./src/controller/AdminController.js";
 import { verifyToken } from "./src/Middleware/VerifyToken.js";
-import { addPolicy } from "./src/controller/PolicyController.js";
+import { addPolicy, getPolicies } from "./src/controller/PolicyController.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/register", registerUser);                                   //works
 app.post("/login", login);                                             //works
@@ -15,7 +17,7 @@ app.post("/admin/addpolicy", addPolicy);                               //works
 
 app.get("/admin/getusers",verifyToken, getAllUsers);                   //works
 app.get("/admin/userpolicies", getUserPolicies);                       //works
-
+app.get("/getpolicies", getPolicies);                                  //works
 
 app.listen(7000, () => {
     connectDb();
